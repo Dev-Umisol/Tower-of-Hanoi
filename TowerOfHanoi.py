54e1c9955a6a056d8f3a5b88d35938ea110496b5
Dev-Umisol <148490822+Dev-Umisol@users.noreply.github.com>
2026-03-26 17:49:26 -0400

    finished tower of hanoi

--- TowerOfHanoi.py
+++ TowerOfHanoi.py
@@ -1,30 +1,45 @@
 def hanoi_solver(total_discs):
+    """
+    Solves the Tower of Hanoi puzzle for a given number of discs
+    Args: total_discs: Number of discs to solve for
+    Returns: A string representing each move taken to solve the puzzle
+    """
+
     rod1, rod2, rod3 = [], [], [] # Setting up rods
     result = ''
     
+    # initializing rod1 with total_discs from largest to smalled
     for n in range(total_discs, 0, -1):
-        rod1.append(n) # Adds the disc to the first rod
+        rod1.append(n) 
         
     current_state = f"{rod1} {rod2} {rod3}\n"
     result += current_state
     
     def move(n, source, target, aux):
+        """Recursively moves n discs from source to target using aux"""
+        
         nonlocal result # nonlocal so that the accumulated string isn't lost every time the function is recursively called
         
         # Recursion Base Case
         if n <= 0:
             return
         
-        move(n - 1, source, aux, target)
+        move(n - 1, source, aux, target) # Moves n - 1 disc out of the way
         disc = source.pop() # Taking disc off rod
         target.append(disc) # Adding it to next rod
         
-        current_state = f"{rod1} {rod2} {rod3}\n"
+        # Update current positions of rods
+        current_state = f"{rod1} {rod2} {rod3}\n" 
         result += current_state
         
-        move(n - 1, target, aux, source)
+        move(n - 1, aux, target, source) # Move n-1 discs to target
         
+    move(total_discs, rod1, rod3, rod2) 
         
-        
-        
-        
+    return result.rstrip('\n') # Get rid of trailing \n
+
+# --> Example Usage <--
+print(hanoi_solver(2))
+print(hanoi_solver(3))
+print(hanoi_solver(4))
+print(hanoi_solver(5))
